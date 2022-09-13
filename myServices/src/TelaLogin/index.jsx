6a753dfd03ac30705d87cb3a8a4ae6cd8d4f3c76
Marcos <marcos.henrique.a7x@@gmail.com>
2022-09-12 22:34:12 -0300

    testes

---
+++ myServices/src/TelaLogin/index.jsx
@@ -0,0 +1,14 @@
+
+import React, {useState} from react;
+
+const [teste, setTeste] = useState("teste do Maarcão")
+export default function teste(){
+console.log("aqui=>", teste)
+    return(
+        <div>
+        <h1>teste</h1>
+        
+        </div>
+    );
+
+}
