6e3610a8c5b57b317e98360998b73317187728ad
Marcos <marcos.henrique.a7x@@gmail.com>
2022-09-13 00:18:17 -0300

    criando estilo

---
+++ myServices/src/TelaLogin/estilos.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const Estilos = StyleSheet.create({
+  inputao: {
+    display: "flex",
+    backgoundColor: "black",
+    width: "100%",
+    heigth: "100%",
+  },
+  butaco: {
+    color: "write",
+  },
+});
+
+export default Estilos;
--- myServices/src/TelaLogin/index.jsx
+++ myServices/src/TelaLogin/index.jsx
@@ -1,14 +1,26 @@
+// import react-native from  react;
+import React from /* { useState }*/ "react";
+import { View, StyleSheet } from "react-native";
+import Estilos from "./estilos.js";
 
-import React, {useState} from react;
-
-const [teste, setTeste] = useState("teste do Maarcão")
-export default function teste(){
-console.log("aqui=>", teste)
-    return(
-        <div>
-        <h1>teste</h1>
-        
-        </div>
-    );
-
+export default function teste() {
+  return (
+    <View>
+      {/* <View> */}
+      <h1>MyServices</h1>
+      <View container>
+        <View item xs={12} style={Estilos.inputao}>
+          <label>Insira o Usuário</label> <br />
+          <input type="text" /> <br />
+          <label>Insira a senha</label>
+          <br />
+          <input type="text" />
+          <br />
+          <button>cadastrar-se</button>
+          <button>login</button>
+        </View>
+      </View>
+      {/* </View> */}
+    </View>
+  );
 }
